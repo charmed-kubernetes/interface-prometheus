45a4be2ce15eb9ce170f06e2462e12693c78fc98
Tilman Baumann <tilman.baumann@canonical.com>
2019-05-22 15:54:33 +0200

    Implemented review suggestions
    Better comments
    Use ingress_address
    Set compat flags

--- provides.py
+++ provides.py
@@ -4,16 +4,34 @@ from charmhelpers.core import hookenv
 from charms.reactive.flags import clear_flag, set_flag
 
 
+'''
+This interface is desiged to be compatible with a previous
+implementation based on RelationBase.
+
+Specifically
+- the `{endpoint_name}.available` flags 
+- the use of `to_publish_raw` to avoid double quoting of the values 
+  as the old interface used plain values here
+'''
+
+
 class PrometheusProvides(Endpoint):
 
     def configure(self, port, path='/metrics',
                   scrape_interval=None, scrape_timeout=None, labels={}):
-        unit_name = hookenv.local_unit()
+        """
+        Interface method to set information provided to remote units
+        """
+
+        # Use our unit name if the label isn't provided
         if labels.get('host') is None:
+            unit_name = hookenv.local_unit()
             labels['host'] = unit_name.replace("/", "-")
         
         for relation in self.relations:
-            relation.to_publish_raw['hostname'] = hookenv.unit_private_ip()
+            relation.to_publish_raw['hostname'] = hookenv.ingress_address(
+                relation.relation_id, hookenv.local_unit()
+            )
             relation.to_publish_raw['port'] = port
             relation.to_publish_raw['metrics_path'] = path
             relation.to_publish['labels'] = labels
@@ -22,13 +40,20 @@ class PrometheusProvides(Endpoint):
             if scrape_timeout is not None:
                 relation.to_publish_raw['scrape_timeout'] = scrape_timeout
 
-    @when('endpoint.{endpoint_name}.departed')
-    def broken(self):
-        if not self.is_joined:
-            clear_flag(self.expand_name('endpoint.{endpoint_name}.available'))
-
     @when('endpoint.{endpoint_name}.joined')
     def available(self):
-        if self.is_joined:
-            set_flag(self.expand_name('endpoint.{endpoint_name}.available'))
+        """
+        Raising the available flag when a unit joined
+        """
+        set_flag(self.expand_name('endpoint.{endpoint_name}.available'))
+        set_flag(self.expand_name('{endpoint_name}.available')) #compatibility
 
+
+    @when('endpoint.{endpoint_name}.departed')
+    def broken(self):
+        """
+        Remove the available flag when the last unit has departed
+        """
+        if not self.is_joined:
+            clear_flag(self.expand_name('endpoint.{endpoint_name}.available'))
+            clear_flag(self.expand_name('{endpoint_name}.available')) #compatibility
--- requires.py
+++ requires.py
@@ -1,26 +1,42 @@
 from charms.reactive import Endpoint
 from charms.reactive import hook, when, when_not
 from charms.reactive.flags import clear_flag, set_flag
-from charmhelpers.core.hookenv import service_name
+from charmhelpers.core.hookenv import service_name, ingress_address
 
+
+'''
+This interface is desiged to be compatible with a previous
+implementation based on RelationBase.
+
+The old `{endpoint_name}.available` flags are maintained
+'''
 
 class PrometheusRequires(Endpoint):
 
     @when('endpoint.{endpoint_name}.changed')
     def changed(self):
+        """
+        Raising the availability flag once we have received the port field from a connected unit.
+        It is convention that remote units signal availability this way.
+        """
         if self.all_joined_units.received['port']:
             set_flag(self.expand_name('endpoint.{endpoint_name}.available'))
+            set_flag(self.expand_name('{endpoint_name}.available')) #compatibility
     
 
     @when('endpoint.{endpoint_name}.departed')
     def broken(self):
+        """
+        Clearing the availability flag once the last unit departed.
+        """
         if not self.is_joined:
             clear_flag(self.expand_name('endpoint.{endpoint_name}.available'))
+            clear_flag(self.expand_name('{endpoint_name}.available')) #compatibility
 
 
     def targets(self):
         """
-        Returns a list of available prometheus targets.
+        Interface method returns a list of available prometheus targets.
             [
                 {
                     'job_name': name_of_job,
@@ -41,18 +57,27 @@ class PrometheusRequires(Endpoint):
                     'job_name': service_name,
                     'targets': [],
                 })
-                host = unit.received['hostname'] or\
-                    unit.received['private-address']
+
+                # If the hostname is not provided we use the informaton from the relation
+                host = (unit.received['hostname'] or
+                    ingress_address(relation.relation_id, unit))
                 port = unit.received['port']
+
+                # Skipping this unit if it isn't ready yet
                 if host and port:
                     service['targets'].append('{}:{}'.format(host, port))
+                else:
+                    continue
+                
                 if unit.received['metrics_path']:
                     service['metrics_path'] = unit.received['metrics_path']
+                if unit.received['labels']:
+                    service['labels'] = unit.received['labels']
+                
+                # Optional fields
                 if unit.received['scrape_interval']:
                     service['scrape_interval'] = unit.received['scrape_interval']
                 if unit.received['scrape_timeout']:
                     service['scrape_timeout'] = unit.received['scrape_timeout']
-                if unit.received['labels']:
-                    service['labels'] = unit.received['labels']
         return [s for s in services.values() if s['targets']]
 
